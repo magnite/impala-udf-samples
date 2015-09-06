4f78c147c1921763e787cbdda89a943ade3a7410
Rob <rob1995@gmail.com>
2015-09-06 02:55:33 -0700

    removed extra line

--- uda-sample-test.cc
+++ uda-sample-test.cc
@@ -11,7 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// this is a test another
 
 #include <iostream>
 #include <math.h>
